5a5b9364e5afe6208dc0d9840f028fe0c87c08da
halking <halking@qq.com>
2019-05-12 22:43:09 +0800

    init project

---
+++ src/main/java/com/hal/nettyj/NettyjApplication.java
@@ -0,0 +1,13 @@
+package com.hal.nettyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyjApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NettyjApplication.class, args);
+  }
+
+}
